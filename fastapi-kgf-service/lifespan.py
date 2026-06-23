67d0166a8bf95deaf20b61d3da3978ee51c9139a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-23 12:23:37 +0300

    black

--- fastapi-kgf-service/lifespan.py
+++ fastapi-kgf-service/lifespan.py
@@ -8,6 +8,9 @@ from core.config import settings
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
-    settings.paths.programs.mkdir(exist_ok=True, parents=True)
+    settings.paths.programs.mkdir(
+        exist_ok=True,
+        parents=True,
+    )
 
     yield None
