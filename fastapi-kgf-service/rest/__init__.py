e5d59957bcacac8aef18c52bf16d67200b421a8f
Ramil Khakimov <bulstray@yandex.ru>
2026-06-30 16:53:52 +0300

    include programs router

--- fastapi-kgf-service/rest/__init__.py
+++ fastapi-kgf-service/rest/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .auth import router as auth_routes
+from .programs import router as programs_routes
 
 
 router = APIRouter()
 
 router.include_router(auth_routes)
+router.include_router(programs_routes)
