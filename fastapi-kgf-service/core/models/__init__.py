48f10f0c95525a3c84508d63572648787926a2bc
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-30 14:56:32 +0300

    include db_helper

--- fastapi-kgf-service/core/models/__init__.py
+++ fastapi-kgf-service/core/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "Base",
     "Program",
     "User",
+    "db_helper",
 )
 
 from .base import Base
 from .program import Program
 from .user import User
+from .db_helper import db_helper
