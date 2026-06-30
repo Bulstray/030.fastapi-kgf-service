184b6542412b71cb82b024f237e7ae0c9ea125e9
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-30 13:33:37 +0300

    add config for db

--- fastapi-kgf-service/core/config.py
+++ fastapi-kgf-service/core/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,10 +19,28 @@ class StoragePath(BaseModel):
     programs: Path = BASE_DIR / "uploads/programs"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(
+            ".env.template",
+            ".env",
+        ),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     paths: StoragePath = StoragePath()
     rest_prefix: RestPrefix = RestPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
