b60dc18ac4f57436c024976cb2edb989ee7cac36
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-30 14:57:39 +0300

    create user Base

--- fastapi-kgf-service/core/schemas/user.py
+++ fastapi-kgf-service/core/schemas/user.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel, ConfigDict
 
 
-class User(BaseModel):
-    model_config = ConfigDict(
-        from_attributes=True,
-    )
-
+class UserBase(BaseModel):
     id: int
     first_name: str
     last_name: str
     email: str
+
+
+class UserRead(BaseModel):
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
