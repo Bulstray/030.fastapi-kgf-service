55e18fd9d7cbcbd4b201709e2fb0e847ec2b4115
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-30 14:56:54 +0300

    create superuser

--- fastapi-kgf-service/app_lifespan.py
+++ fastapi-kgf-service/app_lifespan.py
@@ -4,6 +4,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from core.config import settings
+from core.models import db_helper
+from storage.db.user import crud as user_crud
 
 
 @asynccontextmanager
@@ -13,4 +15,16 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
         parents=True,
     )
 
+    async with db_helper.session_factory() as session:
+        if (
+            await user_crud.get_user_by_email(
+                session,
+                settings.super_user.email,
+            )
+            is None
+        ):
+            await user_crud.create_user(session, settings.super_user)
+
     yield None
+
+    await db_helper.dispose()
