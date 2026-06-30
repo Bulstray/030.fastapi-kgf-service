c7b7469596fe5f4747b45c7ae104c9521e6b9776
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-30 14:00:29 +0300

    user model

---
+++ fastapi-kgf-service/core/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped
+
+from .base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    email: Mapped[str]
+    hashed_password: Mapped[str]
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    is_superuser: Mapped[bool]
