d3c2bb324f12da1d946dd66852148162705944ab
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-30 15:12:22 +0300

    include main_router

--- fastapi-kgf-service/main.py
+++ fastapi-kgf-service/main.py
@@ -5,6 +5,8 @@ from fastapi.staticfiles import StaticFiles
 from core.config import settings, BASE_DIR
 from app_lifespan import lifespan
 
+from rest import router as main_router
+
 app = FastAPI(lifespan=lifespan)
 
 STATIC_PATH = BASE_DIR / "static"
@@ -13,6 +15,8 @@ app.mount(
     StaticFiles(directory=STATIC_PATH),
     name="static",
 )
+
+app.include_router(main_router)
 
 if __name__ == "__main__":
     uvicorn.run(
