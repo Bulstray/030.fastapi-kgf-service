5d9291476b7f7ecafa4ba6319f21680290816efe
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-30 14:43:18 +0300

    add func create user

--- fastapi-kgf-service/storage/db/user/crud.py
+++ fastapi-kgf-service/storage/db/user/crud.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import User
 from sqlalchemy import select
+from core.config import SuperUser
 
 
 async def get_user_by_email(
@@ -11,3 +12,12 @@ async def get_user_by_email(
     stmt = select(User).where(email == User.email)
     result = await session.scalars(stmt)
     return result.first()
+
+
+async def create_user(
+    session: AsyncSession,
+    super_user: SuperUser,
+):
+    user = User(**super_user.model_dump())
+    session.add(user)
+    await session.commit()
